6d98911afd097dd5e27826ff20eca80134e19c58
dev-jaime <jaime.mendes.ixia@gmail.com>
2025-10-27 11:03:09 +0000

    h

--- api/chat.js
+++ api/chat.js
@@ -2,7 +2,7 @@ export default async function handler(req, res) {
   // Permitir CORS
   res.setHeader("Access-Control-Allow-Origin", "https://conversadimercado.web.app");
   res.setHeader("Access-Control-Allow-Methods", "POST, OPTIONS");
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
 
   // Responder preflight (OPTIONS)
   if (req.method === "OPTIONS") {
